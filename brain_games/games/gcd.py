56bafd331a461ed9afd9d01b09235d1f474f80d4
Albert Shubin <albert.shubin@gmail.com>
2025-04-06 22:30:29 +0400

    add new game - gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+RULE = 'Find the greatest common divisor of given numbers.'
+
+
+
+
+def game():
+    first_number = random.randrange(10)
+    second_number = random.randrange(10)
+    question = f'{first_number} {second_number}'
+    return question, str(math.gcd(first_number, second_number))
+
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+from brain_games.engine import engine
+from brain_games.games import gcd
+
+def main():
+    engine(gcd)
+
+if __name__ == "__main__":
+       main()
+
+
